9183ac24adc3e1286ecf8a66c1b0e58db9c003ae
sebasrevuelta <122784773+sebasrevuelta@users.noreply.github.com>
2025-04-02 11:46:50 +0200

    Update my_example.py
    Use parametrized query
    
    Co-authored-by: semgrep-code-sebasrevuelta2[bot] <186034736+semgrep-code-sebasrevuelta2[bot]@users.noreply.github.com>

--- my_example.py
+++ my_example.py
@@ -5,7 +5,8 @@ def sql_print(message: str) -> None:
     commentified_message = textwrap.indent(message, '-- ')
     print(commentified_message)
     with connection.cursor() as cursor:
-        cursor.execute(f'SELECT 1; {commentified_message}')
+        # Use parameterized query to prevent SQL injection
+        cursor.execute('SELECT 1; %s', [commentified_message])
 
 def main():
     sql_print("This is a test message.")
